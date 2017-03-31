452c4e6506fa6c4e5081ec524f7a8c3c4f752aef
Justin Toh <tohjustin@hotmail.com>
2017-03-30 21:20:14 -0700

    Script now also outputs a textfile contain the pixel array of the converted image

--- generatePixelArray.py
+++ generatePixelArray.py
@@ -3,6 +3,12 @@ import sys
 import os
 from PIL import Image
 
+def rgb2hex(r, g, b):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+def rgb2int(r, g, b):
+    return (int(r) << 16) + (int(g) << 8) + int(b)
+
 # Check if user input matches what we want
 if len(sys.argv) < 3:
     print 'ERROR: Missing input param! (eg. "python ' + sys.argv[0] + ' <filename_of_image> <palette_size>")'
@@ -29,7 +35,21 @@ if file_extension not in ['.png', '.jpeg']:
 ########################################
 # Start converting image >_<
 ########################################
+
+# Reduce color depth of image
 img = Image.open(sys.argv[1])
-outputImg = img.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=paletteSize)
+outputImg = img.convert('P', palette=Image.ADAPTIVE, colors=paletteSize).convert('RGB')
 outputImg.save(filename + '-quantized-' + str(paletteSize) + '-colors' + file_extension)
 
+# Output one dimensional pixel array of the converted image in raster order
+pixelList = list(outputImg.getdata())
+outputImg2 = [rgb2int(pixel[0], pixel[1], pixel[2]) for pixel in pixelList]
+f = open(filename + '-outputBinary.txt', 'w')
+f.write('{' + ','.join(str(e) for e in outputImg2) + '}\n')
+f.close()
+
+# Output list of all colors used in converted image
+paletteList = [rgb2int(c[0], c[1], c[2]) for a, c in list(outputImg.getcolors())]
+f = open(filename + '-palette.txt', 'w')
+f.write('{' + ','.join(str(e) for e in paletteList) + '}\n')
+f.close()
